cf80f7402a6f48a3e4c1956db110b5946f5a036b
kyle <kyle@kyle-EP43-UD3L>
2016-08-16 20:18:24 -0700

    Removed bot tokens, Don't want anyone taking them over :|

--- FurryfieldPersonality/src/com/funsize/ffield/bots/Main.java
+++ FurryfieldPersonality/src/com/funsize/ffield/bots/Main.java
@@ -14,12 +14,12 @@ public class Main {
 		
         {
 			
-			toby = new JDABuilder().setBulkDeleteSplittingEnabled(false).setBotToken("MjA2MjcwMDM2ODc1MjgwMzk0.CnSHcQ.pq-wNEPMUYFAWK243CS7H5mL-z4").buildBlocking();
+			toby = new JDABuilder().setBulkDeleteSplittingEnabled(false).setBotToken("").buildBlocking();
 			toby.getAccountManager().setGame(ts);
 			toby.getAccountManager().setNickname(toby.getGuildById("182574336039845889"), "Toby");
 			toby.addEventListener(new TobyAdapter());
 
-			zira = new JDABuilder().setBulkDeleteSplittingEnabled(false).setBotToken("MjEzODM4ODE5MjIzNDA0NTQ0.CpAhdA.8QFhEFhIqcS80TvGcLHpJGqOrIw").buildBlocking();
+			zira = new JDABuilder().setBulkDeleteSplittingEnabled(false).setBotToken("").buildBlocking();
         	zira.getAccountManager().setNickname(zira.getGuildById("182574336039845889"), "Zira");
         	zira.getAccountManager().setGame(zs);
         	zira.addEventListener(new ZiraAdapter());
